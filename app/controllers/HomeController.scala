ec3d5ea8775d007bc43f06ecc52baa5fc3af6c85
tim-koehler <timkoehler97@live.de>
2020-10-26 01:14:10 +0100

    added deps

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,25 +1,30 @@
 package controllers
 
+import com.google.inject.Guice
+import de.htwg.se.scotlandyard.ScotlandYardModule
 import javax.inject._
-
+import de.htwg.se.scotlandyard.aview.Gui
+import de.htwg.se.scotlandyard.aview.tui.Tui
+import de.htwg.se.scotlandyard.controllerComponent.ControllerInterface
+import de.htwg.se.scotlandyard.model.coreComponent.GameMaster
+import de.htwg.se.scotlandyard.model.coreComponent.gameInitializerComponent.GameInitializerInterface
+import de.htwg.se.scotlandyard.model.tuiMapComponent.TuiMapInterface
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
   extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val injector = Guice.createInjector(new ScotlandYardModule)
+
+    val controller = injector.getInstance(classOf[ControllerInterface])
+    val tuiMap = injector.getInstance(classOf[TuiMapInterface])
+    val gameInitializer = injector.getInstance(classOf[GameInitializerInterface])
+
+    GameMaster.initialize()
+    val tui = new Tui(controller, tuiMap)
+    Ok(tui.toString())
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -1,13 +1,14 @@
 lazy val root = (project in file("."))
   .enablePlugins(PlayScala)
   .settings(
-    name := """play-scala-starter-example""",
+    name := """scotland-yard-server""",
     version := "1.0-SNAPSHOT",
     scalaVersion := "2.13.1",
     libraryDependencies ++= Seq(
       guice,
       "com.h2database" % "h2" % "1.4.199",
-      "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+      "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test,
+      "de.htwg.se" %% "scotland-yard" % "0.1.0"
     ),
     scalacOptions ++= Seq(
       "-feature",
