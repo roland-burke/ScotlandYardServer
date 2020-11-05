93611317dce59797a59c37ba87ec080715835c70
tim-koehler <timkoehler97@live.de>
2020-11-05 12:50:24 +0100

    more shit changed

--- app/controllers/SettingsController.scala
+++ app/controllers/SettingsController.scala
@@ -38,7 +38,7 @@ class SettingsController @Inject()(cc: ControllerComponents)(implicit assetsFind
   }
 
   def returnMenuStatusOk(implicit request: Request[_]): Result = {
-    val menuHtml = views.html.main("ScotlandYard")(views.html.settings("Settings", controller.getPlayersList().length.toString, controller.getPlayersList()))
+    val menuHtml = views.html.settings("Settings", controller.getPlayersList().length.toString, controller.getPlayersList())
     Ok(views.html.main("ScotlandYard")(menuHtml))
   }
 
