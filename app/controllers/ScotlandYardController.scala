3f4161fc0e0269f281b1483df877d0c16e2d1384
tim-koehler <timkoehler97@live.de>
2021-01-16 18:38:05 +0100

    changed assets path

--- app/controllers/ScotlandYardController.scala
+++ app/controllers/ScotlandYardController.scala
@@ -2,17 +2,18 @@ package controllers
 
 import akka.actor.ActorSystem
 import forms.ResetPasswordForm
+import play.api.{Environment, Play}
 import play.api.i18n.Messages
 
-import javax.inject.{ Inject, Singleton }
+import javax.inject.{Inject, Singleton}
 import play.api.libs.json.JsValue
 import play.api.libs.streams.ActorFlow
-import play.api.mvc.{ AbstractController, AnyContent, ControllerComponents, Request, WebSocket }
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request, WebSocket}
 import utils.route.Calls
 
 import java.io.File
 import java.util.UUID
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ScotlandYardController @Inject() (cc: ControllerComponents)(implicit system: ActorSystem)
@@ -25,8 +26,8 @@ class ScotlandYardController @Inject() (cc: ControllerComponents)(implicit syste
   }
 }
 
-class ScotlandYardFrontendController @Inject() (scc: SilhouetteControllerComponents)(implicit ex: ExecutionContext) extends SilhouetteController(scc) {
+class ScotlandYardFrontendController @Inject() (scc: SilhouetteControllerComponents,  af: AssetsFinder)(implicit ex: ExecutionContext) extends SilhouetteController(scc) {
   def serveFrontend() = SecuredAction { implicit request: Request[AnyContent] =>
-    Ok.sendFile(new File("./public/ScotlandYardFrontend/index.html"), inline = true)
+    Ok.sendFile(new File(af.path("ScotlandYardFrontend/index.html")), inline = true)
   }
 }
