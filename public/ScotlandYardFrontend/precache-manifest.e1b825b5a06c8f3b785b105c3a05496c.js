093fa951ff6b0ab05210e61f72dda70378dd6675
tim-koehler <timkoehler97@live.de>
2021-01-16 22:39:33 +0100

    updated frontend

---
+++ public/ScotlandYardFrontend/precache-manifest.e1b825b5a06c8f3b785b105c3a05496c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672e857fc5e36c017ad1",
+    "url": "/ScotlandYard/css/about.30a057da.css"
+  },
+  {
+    "revision": "4962de975d825af8bd00",
+    "url": "/ScotlandYard/css/app.289f7047.css"
+  },
+  {
+    "revision": "3e7dfe8ba85eeadb3d4d",
+    "url": "/ScotlandYard/css/chunk-vendors.4cbd925f.css"
+  },
+  {
+    "revision": "7f00415ed67ed014e0d53bbf2df1d08e",
+    "url": "/ScotlandYard/img/Black.7f00415e.svg"
+  },
+  {
+    "revision": "8d9ccbd80c6dc14b6937abe5f7a06712",
+    "url": "/ScotlandYard/img/Bus.8d9ccbd8.svg"
+  },
+  {
+    "revision": "961d9b12f152ea43b631069da8036455",
+    "url": "/ScotlandYard/img/Taxi.961d9b12.svg"
+  },
+  {
+    "revision": "9751aa5aad19b1c490317fb0d64d6b62",
+    "url": "/ScotlandYard/img/Underground.9751aa5a.svg"
+  },
+  {
+    "revision": "0e9b324415bda8b77b4ae2a1e695caf1",
+    "url": "/ScotlandYard/img/black_small.0e9b3244.svg"
+  },
+  {
+    "revision": "985e2be6093fd1600be8024a3b918ec0",
+    "url": "/ScotlandYard/img/bus_small.985e2be6.svg"
+  },
+  {
+    "revision": "e29fdf907a2426f13b1f60cf41101e1b",
+    "url": "/ScotlandYard/img/detective-win.e29fdf90.png"
+  },
+  {
+    "revision": "26b2c88f570b5d3c21c67ab8c9f26d6a",
+    "url": "/ScotlandYard/img/map_large.26b2c88f.webp"
+  },
+  {
+    "revision": "573151a95aad1a029399d75eb51001e5",
+    "url": "/ScotlandYard/img/map_large_small_opacity.573151a9.webp"
+  },
+  {
+    "revision": "91444ed55e6d7cc98f6cda6cef17f400",
+    "url": "/ScotlandYard/img/mrx-win.91444ed5.png"
+  },
+  {
+    "revision": "8a76b141d38e712be4bd7cd04b3e90eb",
+    "url": "/ScotlandYard/img/taxi_small.8a76b141.svg"
+  },
+  {
+    "revision": "de4b32bfde34789caa97bd6c726aaec1",
+    "url": "/ScotlandYard/img/underground_small.de4b32bf.svg"
+  },
+  {
+    "revision": "e53323e1fcc0b70dd046cb4088cef061",
+    "url": "/ScotlandYard/index.html"
+  },
+  {
+    "revision": "672e857fc5e36c017ad1",
+    "url": "/ScotlandYard/js/about.543d0a72.js"
+  },
+  {
+    "revision": "4962de975d825af8bd00",
+    "url": "/ScotlandYard/js/app.7058b008.js"
+  },
+  {
+    "revision": "3e7dfe8ba85eeadb3d4d",
+    "url": "/ScotlandYard/js/chunk-vendors.1c73411d.js"
+  },
+  {
+    "revision": "e23c3301ec7b39effb7c180633b9c520",
+    "url": "/ScotlandYard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ScotlandYard/robots.txt"
+  }
+]);
